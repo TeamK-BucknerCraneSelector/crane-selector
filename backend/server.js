4ae23908dcb7679c27789e5013c4c1181d839c97
Yuanyao Lin <yuanyaolin13@gmail.com>
2025-10-08 21:34:09 -0400

    JSForce package install, starter code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const app = express();
+const jsforce = require('jsforce');
 const cors = require("cors");
+const app = express();
+
+
+// CORS
 const corsOptions = {
   origin: ["http://localhost:5173"],
 };
-
 app.use(cors(corsOptions));
 
+// JSForce
+const conn = new jsforce.Connection();
+
+// Generic AXIOS request w/ dummy data
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "lemon", "strawberry", "pineapple"] });
 });
